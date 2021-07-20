8bade4099acf9d64cd94ef823eb0117c38a3ebe4
abhoward9 <32381914+abhoward9@users.noreply.github.com>
2021-07-20 01:03:37 -0700

    fetches documents by current zip code

--- PartyPace/ViewControllers/FindPartyPaceRidesViewController.swift
+++ PartyPace/ViewControllers/FindPartyPaceRidesViewController.swift
@@ -6,38 +6,92 @@
 //
 
 import UIKit
+import CoreLocation
 import FirebaseDatabase
 import FirebaseFirestore
 
-class FindPartyPaceRidesViewController: UIViewController {
+class FindPartyPaceRidesViewController: UIViewController, CLLocationManagerDelegate {
     
     
     @IBAction func FindRidesButtonPressed(_ sender: Any) {
+        
+        
         findRides()
     }
     
     func findRides() {
 
         let db = Firestore.firestore()
+        
 
-        let docRef = db.collection("Routes").document("99")
-
-        docRef.getDocument { (document, error) in
-            if let document = document, document.exists {
-                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
-                print("Document data: \(dataDescription)")
-            } else {
-                print("Document does not exist")
+        locationManager.requestLocation()
+        let currentLocation = locationManager.location
+        print(currentLocation?.coordinate)
+        let geoCoder = CLGeocoder()
+        
+        geoCoder.reverseGeocodeLocation(currentLocation!) { placemarks, error in
+                if let error = error {
+                    print(error)
+                } else {
+                    
+                    let zipCode = placemarks![0].postalCode!
+                    
+                    let subZip = zipCode.dropLast(2)
+                    var ref: DocumentReference? = nil
+                    
+                    let docRef = db.collection("Routes").document("\(subZip)").collection("\(zipCode)").getDocuments() { (querySnapshot, err) in
+                        if let err = err {
+                            print("Error getting documents: \(err)")
+                        } else {
+                            for document in querySnapshot!.documents {
+                                print("\(document.documentID) => \(document.data())")
+                            }
+                        }
+                    }
+    //                self.db.collection("Routes").document("\(subZip)").collection("\(zipCode)").document("97203").setData([ "test": "\(newride.StartTime!)"]) { err in
+    //                    if let err = err {
+    //                        print("Error writing document: \(err)")
+    //                    } else {
+    //                        print("Document successfully written!")
+    //                    }
+    //                }
+                    
+                    
+                }
             }
-        }
+        
+        
+//        let docRef = db.collection("Routes").document("972").collection("97203").getDocuments() { (querySnapshot, err) in
+//            if let err = err {
+//                print("Error getting documents: \(err)")
+//            } else {
+//                for document in querySnapshot!.documents {
+//                    print("\(document.documentID) => \(document.data())")
+//                }
+//            }
+//        }
+
+//        docRef.getDocument { (document, error) in
+//            if let document = document, document.exists {
+//                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+//                print("Document data: \(dataDescription)")
+//            } else {
+//                print("Document does not exist")
+//            }
+//        }
             
         
     }
+    
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        
+        super.viewDidLoad()
+
+                locationManager.delegate = self
+                locationManager.requestWhenInUseAuthorization()
+                locationManager.requestLocation()
         
     }
     
@@ -52,4 +106,11 @@ class FindPartyPaceRidesViewController: UIViewController {
     }
     */
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("location updated")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+            print("Failed to get users location.")
+        }
 }
